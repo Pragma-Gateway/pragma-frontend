535861d17ed039aa6a3c4af623c6a86e1d70e259
Nicholas Singh <nicholas1singh@gmail.com>
2022-02-02 20:26:09 -0500

    added scatter plot

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -1,98 +1,67 @@
 import React from "react";
-import { StaticMap } from "react-map-gl";
 import DeckGL from "@deck.gl/react";
+import { OrthographicView } from "@deck.gl/core";
 import { ScatterplotLayer } from "@deck.gl/layers";
-
-// const MALE_COLOR = [0, 128, 255];
-// const FEMALE_COLOR = [255, 0, 128];
-
-// // Source data CSV
-// const DATA_URL =
-//   "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scatterplot/manhattan.json"; // eslint-disable-line
-
-// const INITIAL_VIEW_STATE = {
-//   longitude: -74,
-//   latitude: 40.7,
-//   zoom: 11,
-//   maxZoom: 16,
-//   pitch: 0,
-//   bearing: 0,
-// };
-
-// export default function Graph({
-//   data = DATA_URL,
-//   radius = 30,
-//   maleColor = MALE_COLOR,
-//   femaleColor = FEMALE_COLOR,
-//   mapStyle = "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json",
-// }) {
-//   const layers = [
-//     new ScatterplotLayer({
-//       id: "scatter-plot",
-//       data,
-//       radiusScale: radius,
-//       radiusMinPixels: 0.25,
-//       getPosition: (d) => [d[0], d[1], 0],
-//       getFillColor: (d) => (d[2] === 1 ? maleColor : femaleColor),
-//       getRadius: 1,
-//       updateTriggers: {
-//         getFillColor: [maleColor, femaleColor],
-//       },
-//     }),
-//   ];
-
-//   return (
-//     <DeckGL
-//       layers={layers}
-//       initialViewState={INITIAL_VIEW_STATE}
-//       controller={true}
-//     >
-//       <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />
-//     </DeckGL>
-//   );
-// }
 
 import graphData from "./graphData";
 
 const graphDataToPoints = (graphData) => {
-  return graphData.map((d) => [d.x, d.y, 0]);
+  return graphData.map((d) => [d.x, d.y, d.species]);
 };
 
 const MALE_COLOR = [0, 128, 255];
 const FEMALE_COLOR = [255, 0, 128];
+const OTHER_COLOR = [252, 186, 3];
 
 // Source data CSV
 const DATA_URL =
   "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scatterplot/manhattan.json"; // eslint-disable-line
 
 const INITIAL_VIEW_STATE = {
-  longitude: 200,
-  latitude: 100,
-  zoom: 11,
-  maxZoom: 16,
-  pitch: 0,
-  bearing: 0,
+  target: [10, -3, 0],
+  zoom: 4,
+  minZoom: -2,
+  maxZoom: 40,
 };
 
 export default function App({
   data = graphDataToPoints(graphData),
-  radius = 30,
-  maleColor = MALE_COLOR,
-  femaleColor = FEMALE_COLOR,
+  radius = 1,
   mapStyle = "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json",
 }) {
   console.log(data);
+
+  const views = [
+    new OrthographicView({
+      flipY: false,
+      // near: 0.1,
+      // far: 1000,
+    }),
+  ];
+
   const layers = [
     new ScatterplotLayer({
       id: "scatter-plot",
       data,
       radiusScale: radius,
       radiusMinPixels: 0.25,
-      getPosition: (d) => console.log({ x: d[0] }) || [d[0], d[1], 0],
-      getFillColor: (d) => (d[2] === 1 ? maleColor : femaleColor),
+      getPosition: (d) => [d[0], d[1], 0],
+      getFillColor: (d) => {
+        switch (d[2]) {
+          case "setosa":
+            return MALE_COLOR;
+          case "versicolor":
+            return FEMALE_COLOR;
+          default:
+            return OTHER_COLOR;
+        }
+      },
       getRadius: 1,
+      sizeOnUnits: "pixels",
+      // coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
+      // coordinateOrigin: [0, 0, 0],
       updateTriggers: {
-        getFillColor: [maleColor, femaleColor],
+        getFillColor: [MALE_COLOR, FEMALE_COLOR, OTHER_COLOR],
       },
     }),
   ];
@@ -102,12 +71,7 @@ export default function App({
       layers={layers}
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
-    >
-      {/* <StaticMap
-        reuseMaps={false}
-        mapStyle={mapStyle}
-        preventStyleDiffing={false}
-      /> */}
-    </DeckGL>
+      views={views}
+    />
   );
 }
